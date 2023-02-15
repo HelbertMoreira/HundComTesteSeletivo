df8c1b5c49106f88c3c3746a1ac3abf1251b6f19
Helbert Moreira <helbertmspro@gmail.com>
2023-02-15 07:24:33 -0300

    Criando o projeto de postagem com API de autenticação e autorização.

---
+++ HundCom_Postagem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HundCom_Postagem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
